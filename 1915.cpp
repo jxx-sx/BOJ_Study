0e6a57f0a12efebcee7d71905a14b88f2f093af7
jaesang <jaesang00@konkuk.ac.kr>
2022-11-29 15:15:44 +0900

    가장 큰 정사각형 틀렸습니다

---
+++ 1915.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m;
+    cin >> n >> m;
+    int arr[n][m];
+    char c;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> c;
+            if (c == '0')
+                arr[i][j] = 0;
+            else
+                arr[i][j] = 1;
+        }
+    }
+    int ans = 0;
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            arr[i][j] += min(arr[i][j - 1], min(arr[i - 1][j - 1], arr[i - 1][j]));
+            ans = max(ans, arr[i][j]);
+        }
+    }
+    cout << ans * ans;
+}
