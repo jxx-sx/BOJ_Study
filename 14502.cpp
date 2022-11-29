0d5e9e00ee5293dc0b8e7a9509c7defc43657146
jaesang <jaesang00@konkuk.ac.kr>
2022-11-29 19:45:42 +0900

    연구소 틀렸습니다

--- 14502.cpp
+++ 14502.cpp
@@ -8,8 +8,6 @@ int **arr, **tmp;
 vector<pair<int, int>> v;
 
 void dfs(int x, int y) {
-    if (tmp[x][y] != 2)
-        return;
     if (x > 0) {
         if (tmp[x - 1][y] == 0) {
             tmp[x - 1][y] = 2;
@@ -39,8 +37,7 @@ void dfs(int x, int y) {
 void dfs() {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (tmp[i][j] != 3)
-                tmp[i][j] = arr[i][j];
+            tmp[i][j] = arr[i][j];
         }
     }
 
@@ -59,23 +56,25 @@ void dfs() {
 }
 
 void before_dfs() {
-    for (int i = 0; i < n * m; i++) {
+    for (int i = 0; i < n * m - 2; i++) {
         if (arr[i / m][i % m] == 0) {
-            tmp[i / m][i % m] = 3;
-            for (int j = i + 1; j < n * m; j++) {
+            arr[i / m][i % m] = 1;
+            for (int j = i + 1; j < n * m - 1; j++) {
                 if (arr[j / m][j % m] == 0) {
-                    tmp[j / m][j % m] = 3;
+                    arr[j / m][j % m] = 1;
                     for (int k = j + 1; k < n * m; k++) {
                         if (arr[k / m][k % m] == 0) {
-                            tmp[k / m][k % m] = 3;
+                            arr[k / m][k % m] = 1;
+
                             dfs();
-                            tmp[k / m][k % m] = 0;
+
+                            arr[k / m][k % m] = 0;
                         }
                     }
-                    tmp[j / m][j % m] = 0;
+                    arr[j / m][j % m] = 0;
                 }
             }
-            tmp[i / m][i % m] = 0;
+            arr[i / m][i % m] = 0;
         }
     }
 }
