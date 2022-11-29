970d0fb1caed8dfa5b9e6e2963d6bfa8059d764d
jaesang <jaesang00@konkuk.ac.kr>
2022-11-29 15:18:16 +0900

    가장 큰 정사각형 solve

--- solved/1915.cpp
+++ solved/1915.cpp
@@ -28,8 +28,10 @@ int main() {
 
     for (int i = 1; i < n; i++) {
         for (int j = 1; j < m; j++) {
-            arr[i][j] += min(arr[i][j - 1], min(arr[i - 1][j - 1], arr[i - 1][j]));
-            ans = max(ans, arr[i][j]);
+            if (arr[i][j] == 1) {
+                arr[i][j] += min(arr[i][j - 1], min(arr[i - 1][j - 1], arr[i - 1][j]));
+                ans = max(ans, arr[i][j]);
+            }
         }
     }
     cout << ans * ans;
