8cbd7617604a7274ae2b1ded2daba13bbae0357a
jaesang <jaesang00@konkuk.ac.kr>
2022-11-29 19:53:21 +0900

    연구소 solve

--- solved/14502.cpp
+++ solved/14502.cpp
@@ -4,48 +4,41 @@
 using namespace std;
 
 int ans = 0, n, m, dfs_count;
-int **arr, **tmp;
+int arr[8][8], tmp[8][8];
+
 vector<pair<int, int>> v;
 
 void dfs(int x, int y) {
-    if (x > 0) {
-        if (tmp[x - 1][y] == 0) {
-            tmp[x - 1][y] = 2;
+    tmp[x][y] = 2;
+
+    if (x > 0)
+        if (tmp[x - 1][y] == 0)
             dfs(x - 1, y);
-        }
-    }
-    if (y > 0) {
-        if (tmp[x][y - 1] == 0) {
-            tmp[x][y - 1] = 2;
+
+    if (y > 0)
+        if (tmp[x][y - 1] == 0)
             dfs(x, y - 1);
-        }
-    }
-    if (x + 1 < n) {
-        if (tmp[x + 1][y] == 0) {
-            tmp[x + 1][y] = 2;
+
+    if (x + 1 < n)
+        if (tmp[x + 1][y] == 0)
             dfs(x + 1, y);
-        }
-    }
-    if (y + 1 < n) {
-        if (tmp[x][y + 1] == 0) {
-            tmp[x][y + 1] = 2;
+
+    if (y + 1 < m)
+        if (tmp[x][y + 1] == 0)
             dfs(x, y + 1);
-        }
-    }
 }
 
 void dfs() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
             tmp[i][j] = arr[i][j];
-        }
-    }
 
     for (int i = 0; i < v.size(); i++) {
         dfs(v[i].first, v[i].second);
     }
 
     dfs_count = 0;
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (tmp[i][j] == 0)
@@ -81,11 +74,7 @@ void before_dfs() {
 
 void init() {
     cin >> n >> m;
-    arr = new int *[n];
-    tmp = new int *[n];
     for (int i = 0; i < n; i++) {
-        arr[i] = new int[m];
-        tmp[i] = new int[m];
         for (int j = 0; j < m; j++) {
             cin >> arr[i][j];
             if (arr[i][j] == 2) {
