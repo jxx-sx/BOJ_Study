03ca19f7063d54c352be880fbcca45818ef4286e
jaesang <jaesang00@konkuk.ac.kr>
2022-12-09 15:14:54 +0900

    보석 도둑 틀렸습니다

---
+++ 1202.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+int n, k;
+vector<pair<int, int>> v;
+vector<int> bag;
+priority_queue<int> pq;
+long long ans = 0;
+
+void solve() {
+    int i = 0;
+    for (auto a : bag) {
+        for (; i < v.size(); i++) {
+            if (v[i].first < a)
+                pq.push(v[i].second);
+            else
+                break;
+        }
+        if (!pq.empty()) {
+            ans += pq.top();
+            pq.pop();
+        }
+    }
+    cout << ans;
+    return;
+}
+
+void init() {
+    int s, e;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> s >> e;
+        v.push_back({s, e});
+    }
+    for (int i = 0; i < k; i++) {
+        cin >> s;
+        bag.push_back(s);
+    }
+
+    sort(v.begin(), v.end());
+    sort(bag.begin(), bag.end());
+    solve();
+    return;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    init();
+    return 0;
+}
