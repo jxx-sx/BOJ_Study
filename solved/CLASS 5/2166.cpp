85b04f0ad3f9870523af1cfceee15735e1c58c6c
jaesang <jaesang00@konkuk.ac.kr>
2022-12-09 20:02:49 +0900

    다각형의 면적 solve

--- solved/CLASS 5/2166.cpp
+++ solved/CLASS 5/2166.cpp
@@ -3,21 +3,21 @@
 
 using namespace std;
 int n;
-int xx[10001], yy[10001];
+long long xx[10001], yy[10001];
 
 void solve() {
     long long ans = 0;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         ans += xx[i] * yy[i + 1];
+
+    for (int i = 0; i < n; i++)
         ans -= xx[i + 1] * yy[i];
-    }
+
     ans = abs(ans);
-    int tmp = 0;
-    if (ans % 2 == 1)
-        tmp = 5;
-    round(ans);
-    cout << ans / 2 << '.' << tmp;
+    cout << fixed;
+    cout.precision(1);
+    cout << ans / 2.0;
 }
 
 void init() {
