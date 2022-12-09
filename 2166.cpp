0f86d72d5e7517bdaafa0f15dbcccf43ef14ab06
jaesang <jaesang00@konkuk.ac.kr>
2022-12-09 19:08:46 +0900

    다각형의 면적 틀렸습니다

--- 2166.cpp
+++ 2166.cpp
@@ -6,17 +6,15 @@ int n;
 int xx[10000], yy[10000];
 
 void solve() {
-    int x = xx[n - 1];
-    int y = yy[n - 1];
-    int a, b, c; // a*x + b*y + c = 0
-    double tmp;
+    int x = xx[n - 1]; // fixed
+    int y = yy[n - 1]; // fixed
     long double ans = 0.0;
-    for (int i = 0; i < n - 2; i++) {
-        tmp = abs(xx[i] * yy[i + 1] + xx[i + 1] * y + x * yy[i] - xx[i + 1] * yy[i] - x * yy[i + 1] - xx[i] * y) / 2.0;
-        ans += tmp * 10;
-    }
 
-    abs(ans);
+    for (int i = 0; i < n - 2; i++) {
+        ans += (xx[i] * yy[i + 1] + xx[i + 1] * y + x * yy[i] - xx[i + 1] * yy[i] - x * yy[i + 1] - xx[i] * y) * 5.0;
+    }
+    round(ans);
+
     cout << fixed;
     cout.precision(1);
     cout << round(ans) / 10.0;
