4796d8c9bbbde7698b077676b6ddd864f3cc170d
jaesang <jaesang00@konkuk.ac.kr>
2022-12-10 13:23:32 +0900

    세부 틀렸습니다

--- 13905.cpp
+++ 13905.cpp
@@ -1,55 +1,72 @@
+#include <algorithm>
 #include <iostream>
 #include <queue>
 #include <vector>
+
 using namespace std;
+
+int n, m, s, e;
+vector<pair<int, pair<int, int>>> edges;
+int p[100001];
+int r[100001];
+
+int find_root(int x) {
+    if (p[x] == x)
+        return x;
+    return p[x] = find_root(p[x]);
+}
+
+void union_root(int a, int b) {
+    a = find_root(a);
+    b = find_root(b);
+    if (r[a] > r[b]) {
+        r[a]++;
+        p[b] = a;
+    } else {
+        r[b]++;
+        p[a] = b;
+    }
+}
+
+void solve() {
+    int h1, h2, k;
+    int ans = 1000000;
+    while (find_root(s) != find_root(e)) {
+        h1 = edges.back().second.first;
+        h2 = edges.back().second.second;
+        k = edges.back().first;
+        edges.pop_back();
+        
+        if (find_root(h1) != find_root(h2)) {
+            union_root(h1, h2);
+            ans = min(ans, k);
+        }
+    }
+    cout << ans;
+}
+
+void init() {
+    cin >> n >> m >> s >> e;
+    int a, b, c;
+    for (int i = 1; i <= n; i++) {
+        p[i] = i;
+        r[i] = 0;
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b >> c;
+        edges.push_back({a, {b, c}});
+    }
+
+    sort(edges.begin(), edges.end());
+
+    solve();
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n, m, s, e, h1, h2, k;
-    int ans = 100000000;
-    cin >> n >> m >> s >> e;
-    vector<pair<int, int>> edges[n + 1];
-    priority_queue<pair<int, int>> pq;
-    bool check[n + 1];
-
-    for (int i = 0; i < n + 1; i++) {
-        check[i] = false;
-    }
-
-    for (int i = 0; i < m; i++) {
-        cin >> h1 >> h2 >> k;
-        edges[h1].push_back({k, h2});
-        edges[h2].push_back({k, h1});
-    }
-
-    for (int i = 0; i < edges[h2].size(); i++) {
-        pq.push(edges[h2][i]);
-    }
-
-    check[h1] = true;
-
-    while (!pq.empty()) {
-        pair<int, int> cur = pq.top();
-        pq.pop();
-
-        if (cur.second == h1) {
-            ans = (ans < cur.first ? ans : cur.first);
-            cout << ans;
-            break;
-        }
-
-        if (!check[cur.second]) {
-            check[cur.second] = true;
-            ans = (ans < cur.first ? ans : cur.first);
-            for (int i = 0; i < edges[cur.second].size(); i++) {
-                if (!check[edges[cur.second][i].second]) {
-                    pq.push(edges[cur.second][i]);
-                }
-            }
-        }
-    }
-
+    init();
     return 0;
 }
