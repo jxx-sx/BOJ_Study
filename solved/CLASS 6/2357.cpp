d24c76355ba04a44251700938305a28507da0f48
jaesang <jaesang00@konkuk.ac.kr>
2022-12-29 15:09:50 +0900

    최솟값과 최댓값 solve

--- solved/CLASS 6/2357.cpp
+++ solved/CLASS 6/2357.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#define MAX_INT 1000000001
 
 using namespace std;
 vector<pair<int, int>> tree;
@@ -8,31 +9,21 @@ int n, m;
 
 pair<int, int> find_min_max(int index, int s, int e, int l, int r) {
     if (e < l or r < s)
-        return make_pair(-1, -1);
+        return make_pair(MAX_INT, -1);
     if (l <= s and e <= r)
         return tree[index];
     pair<int, int> l_c = find_min_max(index * 2, s, (s + e) / 2, l, r);
     pair<int, int> r_c = find_min_max(index * 2 + 1, (s + e) / 2 + 1, e, l, r);
-    if (l_c == make_pair(-1, -1))
-        return r_c;
-    else if (r_c == make_pair(-1, -1))
-        return l_c;
-    else {
-        int l_data = tree_data[l_c.first] < tree_data[r_c.first] ? l_c.first : r_c.first;
-        int r_data = tree_data[l_c.second] > tree_data[r_c.second] ? l_c.second : r_c.second;
-        return make_pair(l_data, r_data);
-    }
+    return {min(l_c.first, r_c.first), max(l_c.second, r_c.second)};
 }
 
 pair<int, int> tree_init(int index, int s, int e) {
     if (s == e)
-        return tree[index] = {s, s}; //{min_index, max_index}
+        return tree[index] = {tree_data[s], tree_data[s]}; //{min, max}
 
     pair<int, int> l_c = tree_init(index * 2, s, (s + e) / 2);
     pair<int, int> r_c = tree_init(index * 2 + 1, (s + e) / 2 + 1, e);
-    int l = tree_data[l_c.first] <= tree_data[r_c.first] ? l_c.first : r_c.first;
-    int r = tree_data[l_c.second] >= tree_data[r_c.second] ? l_c.second : r_c.second;
-    return tree[index] = {l, r};
+    return tree[index] = {min(l_c.first, r_c.first), max(l_c.second, r_c.second)};
 }
 
 void init() {
@@ -52,7 +43,7 @@ void solve() {
     for (int i = 0; i < m; i++) {
         cin >> a >> b;
         ans = find_min_max(1, 0, n - 1, a - 1, b - 1);
-        cout << tree_data[ans.first] << ' ' << tree_data[ans.second] << '\n';
+        cout << ans.first << ' ' << ans.second << '\n';
     }
 
     return;
