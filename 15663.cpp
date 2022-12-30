bcb44ffc4626c10dcc8f45bad00d3ba6a1e73627
jaesang <jaesang00@konkuk.ac.kr>
2022-12-30 18:23:47 +0900

    N과 M (9) 틀렸습니다

--- 15663.cpp
+++ 15663.cpp
@@ -3,47 +3,58 @@
 #include <vector>
 
 using namespace std;
-
 int n, m;
-bool check[8];
-vector<int> ans;
-vector<int> datas;
+vector<bool> check = vector<bool>(8, false);
+vector<int> datas, ans;
+vector<int> isUsed[8];
+
 void dfs(int x, int l) {
     if (check[x])
         return;
+
+    if (!isUsed[l].empty())
+        if (isUsed[l].back() == datas[x])
+            return;
+
     check[x] = true;
     ans.push_back(x);
+    isUsed[l].push_back(datas[x]);
+    l += 1;
+
     if (l == m) {
-        for (int i = 0; i < m; i++) {
-            cout << datas[ans[i]] << ' ';
-        }
+        for (auto i : ans)
+            cout << datas[i] << ' ';
         cout << '\n';
     } else {
         for (int i = 0; i < n; i++) {
-            dfs(i, l + 1);
+            dfs(i, l);
         }
     }
+
     check[x] = false;
     ans.pop_back();
+
     return;
 }
+
 void init() {
-    for (int i = 0; i < 8; i++) {
-        check[i] = false;
-    }
     cin >> n >> m;
-    int tmp;
+    int num;
     for (int i = 0; i < n; i++) {
-        cin >> tmp;
-        datas.push_back(tmp);
+        cin >> num;
+        datas.push_back(num);
     }
     sort(datas.begin(), datas.end());
 }
+
+void solve() {
+    for (int i = 0; i < n; i++)
+        dfs(i, 0);
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     init();
-    for (int i = 0; i < n; i++) {
-        dfs(i, 1);
-    }
+    solve();
 }
