ed3191f50ff8b13aa94f191d48d1db3d14c76270
jaesang <jaesang00@konkuk.ac.kr>
2023-01-02 18:00:46 +0900

    숨바꼭질 solve

--- 1697.cpp
+++ 1697.cpp
@@ -1,43 +1,47 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 
 using namespace std;
 int n, k, ans = 0;
 vector<bool> is_visit = vector<bool>(100100, false);
-vector<int> v;
+queue<pair<int, int>> q;
 
 void init() {
     cin >> n >> k;
-    v.push_back(n);
+    q.push({n, 0});
     is_visit[n] = true;
     return;
 }
 
 void solve() {
-    while (true) {
-        vector<int> tmp;
-        for (auto a : v) {
-            if (a == k) {
-                cout << ans;
-                return;
-            } else {
-                if (!is_visit[a + 1] and a + 1 < 100100) {
-                    tmp.push_back(a + 1);
+    while (!q.empty()) {
+        pair<int, int> cur = q.front();
+        q.pop();
+        int a = cur.first;
+        if (a == k) {
+            cout << cur.second;
+            break;
+        } else {
+            if (a + 1 < 100100)
+                if (!is_visit[a + 1]) {
+                    q.push({a + 1, cur.second + 1});
                     is_visit[a + 1] = true;
                 }
-                if (!is_visit[a - 1] and a > 1) {
-                    tmp.push_back(a - 1);
+            if (a - 1 >= 0)
+                if (!is_visit[a - 1]) {
+                    q.push({a - 1, cur.second + 1});
                     is_visit[a - 1] = true;
                 }
-                if (!is_visit[a * 2] and a * 2 < 100100) {
-                    tmp.push_back(a * 2);
+            if (a * 2 < 100100)
+                if (!is_visit[a * 2]) {
+                    q.push({a * 2, cur.second + 1});
                     is_visit[a * 2] = true;
                 }
-            }
         }
-        ans += 1;
-        v = tmp;
     }
+
+    return;
 }
 
 int main() {
