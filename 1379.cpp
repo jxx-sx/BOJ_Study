cd52176a53eb81933098b596d8fa6c0b156fb601
jaesang <jaesang00@konkuk.ac.kr>
2023-03-16 12:21:07 +0900

    틀렸습니다 : 강의실 2

---
+++ 1379.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+typedef pair<pair<int, int>, int> Lecture;
+priority_queue<Lecture, vector<Lecture>, greater<Lecture>> lecture;
+int ans[100001];
+int room_cnt = 1, n;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> room;
+
+void init() {
+    int a, b, c;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b >> c;
+        lecture.push({{c, b}, a});
+    }
+    room.push({0, 1});
+    return;
+}
+
+void solve() {
+    while (!lecture.empty()) {
+        if (room.top().first <= lecture.top().first.second) {
+            int cur = room.top().second;
+            ans[lecture.top().second] = cur;
+            room.pop();
+            room.push({lecture.top().first.first, cur});
+        } else {
+            room_cnt++;
+            room.push({lecture.top().first.first, room_cnt});
+            ans[lecture.top().second] = room_cnt;
+        }
+        lecture.pop();
+    }
+    cout << room_cnt << '\n';
+    for (int i = 1; i <= n; i++)
+        cout << ans[i] << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    init();
+    solve();
+
+    return 0;
+}
