812434b1a44084ea09e5ce095eda05177c215ded
jaesang <jaesang00@konkuk.ac.kr>
2023-08-01 13:05:59 +0900

    MLE: 서로 다른 수와 쿼리 2

--- 14898.cpp
+++ 14898.cpp
@@ -1,82 +1,96 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#define pb push_back
 #define N 1000001
+#define NN                                                                                                                                                     \
+    tree.size();                                                                                                                                               \
+    tree.pb(Node())
 
 using namespace std;
 
 struct Node {
-    int d;
-    Node *l, *r;
-    Node() : d(0), l(nullptr), r(nullptr) {}
-    Node(Node *t) : d(t->d), l(t->l), r(t->r) {}
-} tree[N];
+    int d, l, r;
+    Node() : d(0), l(0), r(0) {}
+    void copy(Node *t) {
+        d = t->d;
+        l = t->l;
+        r = t->r;
+    }
+};
 
 int n, q, last;
 int arr[N], pre[N], comp[N];
+vector<Node> tree;
 
-void tree_init(int s, int e, Node *cur) {
+void tree_init(int s, int e, int cur) {
     if (s == e)
         return;
-    cur->l = new Node();
-    cur->r = new Node();
-    tree_init(s, (s + e) / 2, cur->l);
-    tree_init((s + e) / 2 + 1, e, cur->r);
+    tree[cur].l = NN;
+    tree[cur].r = NN;
+    tree_init(s, (s + e) / 2, tree[cur].l);
+    tree_init((s + e) / 2 + 1, e, tree[cur].r);
 }
 
-void tree_init1(int k, int s, int e, Node *cur, Node *before) {
-    cur->d = before->d + 1;
-    if (s == e)
-        return;
-    if (k > (s + e) / 2) {
-        cur->l = before->l;
-        cur->r = new Node();
-        tree_init1(k, (s + e) / 2 + 1, e, cur->r, before->r);
-    } else {
-        cur->l = new Node();
-        cur->r = before->r;
-        tree_init1(k, s, (s + e) / 2, cur->l, before->l);
-    }
-}
-
-void update(int k, int s, int e, Node *cur) {
-    cur->d--;
+void tree_init1(int k, int s, int e, int cur, int before) {
+    tree[cur].d = tree[before].d + 1;
     if (s == e)
         return;
     if (k > (s + e) / 2) {
-        cur->r = new Node(cur->r);
-        update(k, (s + e) / 2 + 1, e, cur->r);
+        tree[cur].l = tree[before].l;
+        tree[cur].r = NN;
+        tree_init1(k, (s + e) / 2 + 1, e, tree[cur].r, tree[before].r);
     } else {
-        cur->l = new Node(cur->l);
-        update(k, s, (s + e) / 2, cur->l);
+        tree[cur].l = NN;
+        tree[cur].r = tree[before].r;
+        tree_init1(k, s, (s + e) / 2, tree[cur].l, tree[before].l);
     }
 }
 
-int query(int l, int r, int s, int e, Node *cur) {
+void update(int k, int s, int e, int cur) {
+    tree[cur].d--;
+    if (s == e)
+        return;
+    if (k > (s + e) / 2) {
+        Node *tmp = &tree[tree[cur].r];
+        tree[cur].r = NN;
+        tree[tree[cur].r].copy(tmp);
+        update(k, (s + e) / 2 + 1, e, tree[cur].r);
+    } else {
+        Node *tmp = &tree[tree[cur].l];
+        tree[cur].l = NN;
+        tree[tree[cur].l].copy(tmp);
+        update(k, s, (s + e) / 2, tree[cur].l);
+    }
+}
+
+int query(int l, int r, int s, int e, int cur) {
     if (e < l or r < s)
         return 0;
     if (l <= s and e <= r)
-        return cur->d;
-    return query(l, r, s, (s + e) / 2, cur->l) + query(l, r, (s + e) / 2 + 1, e, cur->r);
+        return tree[cur].d;
+    return query(l, r, s, (s + e) / 2, tree[cur].l) + query(l, r, (s + e) / 2 + 1, e, tree[cur].r);
 }
 
 void init() {
     cin >> n;
-    for (int i = 1; i <= n; i++) {
+    tree.pb(Node());
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
-        comp[i - 1] = arr[i];
+        comp[i] = arr[i];
+        tree.pb(Node());
     }
     cin >> q;
     sort(comp, comp + n);
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
         arr[i] = lower_bound(comp, comp + n, arr[i]) - comp;
 
-    tree_init(1, n, tree);
-    for (int i = 1; i <= n; i++) {
-        tree_init1(i, 1, n, tree + i, tree + i - 1);
+    tree_init(1, n, 0);
+    for (int i = 0; i < n; i++) {
+        tree_init1(i + 1, 1, n, i + 1, i);
         if (pre[arr[i]])
-            update(pre[arr[i]], 1, n, tree + i);
-        pre[arr[i]] = i;
+            update(pre[arr[i]], 1, n, i + 1);
+        pre[arr[i]] = i + 1;
     }
 }
 
@@ -84,7 +98,7 @@ void solve() {
     int x, r;
     while (q--) {
         cin >> x >> r;
-        last = query(x + last, r, 1, n, tree + r);
+        last = query(x + last, r, 1, n, r);
         cout << last << '\n';
     }
 }
