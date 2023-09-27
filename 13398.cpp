02c31d1750e0b150d680dbb7315f6d65486a1ff5
Jaesang <59428259+jxx-sx@users.noreply.github.com>
2023-09-27 13:09:15 +0900

    wa: 연속합 2

--- 13398.cpp
+++ 13398.cpp
@@ -17,9 +17,9 @@ void init() {
 void solve() {
     if (arr[1] > 0)
         dp[0][1] = arr[1];
-    mx = max(0, arr[1]);
+    mx = arr[1];
     for (int i = 2; i <= n; i++) {
-        dp[0][i] = max(dp[0][i - 1] + arr[i], 0);
+        dp[0][i] = max(dp[0][i - 1] + arr[i], arr[i]);
         dp[1][i] = max({dp[0][i - 2] + arr[i], dp[0][i - 1], dp[1][i - 1] + arr[i]});
         mx = max({mx, dp[0][i], dp[1][i]});
     }
