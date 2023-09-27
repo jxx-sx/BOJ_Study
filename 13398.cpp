cbb7162f99e1423c0db2cddbfc2c2e84cbda1aa9
Jaesang <59428259+jxx-sx@users.noreply.github.com>
2023-09-27 13:08:49 +0900

    틀렸습니다: 연속합 2

---
+++ 13398.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int arr[100001], dp[2][100001];
+int n, mx;
+
+void init() {
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+}
+
+void solve() {
+    if (arr[1] > 0)
+        dp[0][1] = arr[1];
+    mx = max(0, arr[1]);
+    for (int i = 2; i <= n; i++) {
+        dp[0][i] = max(dp[0][i - 1] + arr[i], 0);
+        dp[1][i] = max({dp[0][i - 2] + arr[i], dp[0][i - 1], dp[1][i - 1] + arr[i]});
+        mx = max({mx, dp[0][i], dp[1][i]});
+    }
+    cout << mx;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    init();
+    solve();
+
+    return 0;
+}
