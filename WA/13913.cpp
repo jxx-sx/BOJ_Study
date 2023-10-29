9b25649c0b653f73d463818ee36f7bd862d958e3
Jaesang <59428259+jxx-sx@users.noreply.github.com>
2023-10-29 21:23:43 +0900

    WA 숨바꼭질 4

--- WA/13913.cpp
+++ WA/13913.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-pair<int, int> arr[100001]; // depth, origin
+pair<int, int> arr[200001]; // depth, origin
 queue<int> q;
 int n, k;
 
 void query(int x, int cur) {
     if (x < 0)
         return;
-    if (x > 100000)
+    if (x > 200000)
         return;
     if (arr[x].second)
         return;
@@ -29,10 +29,6 @@ void init() {
 void solve() {
     while (q.front() != k) {
         query(q.front() - 1, q.front());
-        if (k < q.front()) {
-            q.pop();
-            continue;
-        }
         query(2 * q.front(), q.front());
         query(q.front() + 1, q.front());
         q.pop();
