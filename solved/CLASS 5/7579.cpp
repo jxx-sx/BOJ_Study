95d27e296e4b17987bde3bd29d9f60716559c03c
Jaesang <59428259+jxx-sx@users.noreply.github.com>
2024-02-12 17:21:53 +0900

    AC 앱

--- solved/CLASS 5/7579.cpp
+++ solved/CLASS 5/7579.cpp
@@ -1,56 +1,41 @@
-#include <algorithm>
+#include <cassert>
 #include <iostream>
-#include <vector>
+
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
 
 using namespace std;
 
-int n, m;
-long long sumM = 0, sumC = 0;
-vector<int> vM, vC;
+int n, m, ans;
+pair<int, int> arr[101];
+int dp[101][10001];
+
+void init() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i].first;
+        ans += arr[i].first;
+    }
+    for (int i = 1; i <= n; i++)
+        cin >> arr[i].second;
+}
 
 void solve() {
-    vector<int> v[sumC + 1];
-
-    for (int i = 0; i < n + 1; i++)
-        v[0].push_back(0);
-
-    for (int j = 1; j <= sumC; j++) {
-        for (int i = 0; i < n + 1; i++) {
-            if (i == 0) {
-                v[j].push_back(0);
-                continue;
-            }
-            if (vC[i - 1] > j)
-                v[j].push_back(v[j].back());
-            else {
-                int tmp;
-                tmp = max(v[j - vC[i - 1]][i - 1] + vM[i - 1], v[j].back());
-                if (tmp >= m) {
-                    cout << j;
-                    return;
-                }
-                v[j].push_back(tmp);
+    for (int i = 0; i < 10001; i++) {
+        for (int j = 1; j <= n; j++) {
+            dp[j][i] = dp[j - 1][i];
+            if
+                7579.cpp(i > 0) dp[j][i] = max(dp[j][i], dp[j][i - 1]);
+            if (i >= arr[j].second)
+                dp[j][i] = max(dp[j][i], dp[j - 1][i - arr[j].second] + arr[j].first);
+            if (dp[j][i] >= m) {
+                cout << i;
+                return;
             }
         }
     }
-}
-
-void init() {
-    int inp;
-    cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-        cin >> inp;
-        vM.push_back(inp);
-        sumM += inp;
-    }
-
-    for (int i = 0; i < n; i++) {
-        cin >> inp;
-        vC.push_back(inp);
-        sumC += inp;
-    }
-
-    solve();
+    // cout << ans;
 }
 
 int main() {
@@ -58,4 +43,7 @@ int main() {
     cin.tie(NULL);
 
     init();
+    solve();
+
+    return 0;
 }
